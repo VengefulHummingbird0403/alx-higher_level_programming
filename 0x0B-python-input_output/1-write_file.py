dd9dab0a83387d331811576e0ccc946f295c4fb7
VengefulHummingbird0403 <hoveronoh@gmail.com>
2023-11-07 19:57:11 -0800

    initial_Push

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a file-writing function."""
+
+
+def write_file(filename="", text=""):
+    """
+    Args:
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
+    Returns:
+        characters written.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
